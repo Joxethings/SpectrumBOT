92ca255a61dd69717f664c802257ca6e4fdc031c
Joxethings <46269101+Joxethings@users.noreply.github.com>
2018-12-31 00:05:19 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,6 +10,24 @@ client.on('message', message => {
   if (message.content === 'ping') {
     message.reply('PONG');
     }
+   if (bot.directMessages[channelID]) {
+        if (message.toLowerCase().startsWith("!auth")) {
+
+            bot.sendMessage({
+                to: userID,
+                embed: {
+                    "title": "Authorization!",
+                    "description": "Please take a second and click this link: [Authorize me!](https://login.eveonline.com/oauth/authorize/?response_type=code&client_id=7da1c05e886c4404a8d54c5c4c37eb4f&redirect_uri=https://localhost/callback&scope=publicData)\nI will get back to you when you have given me the permissions i need.",
+                    "color": 2615919,
+                    "timestamp": date,
+                    "footer": {
+                        "text": "FLSP authorization"
+                    },
+                    "image": {
+                        "url": "https://i.ytimg.com/vi/mZ1YgQGQxE8/maxresdefault.jpg"
+                    }
+                }
+            });
 });
   
   client.login(process.env.BOT_TOKEN);
