547d9bba3e8875aa6b0a6f2a74311a9dfccf395d
Joxethings <46269101+Joxethings@users.noreply.github.com>
2018-12-31 00:09:39 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,13 @@
 const Discord = require('discord.js');
 const keep_alive = require('./keep_alive.js')
 const client = new Discord.Client();
+var userID;
 
 client.on('ready', () => {
   console.log('I am ready!');
 });
 
-client.on('message', message => {
+client.on('message', function (user, userID, message){
   if (message.content === 'ping') {
     message.reply('PONG');
     }
